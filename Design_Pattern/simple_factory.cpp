ab3a8be8089231aab3d5d2d9560f724ac65fec4e
wellupgeek <well_geek@foxmail.com>
2021-06-22 17:50:39 +0800

    工厂方法模式

--- Design_Pattern/simple_factory.cpp
+++ Design_Pattern/simple_factory.cpp
@@ -39,58 +39,100 @@ enum SHOES_TYPE
     ADIDAS
 };
 
-class ShoesFactory
-{
+// class ShoesFactory
+// {
+// public:
+//     // 根据鞋子类型创建对应的鞋子对象
+//     Shoes *CreateShoes(SHOES_TYPE type) {
+//         switch (type) {
+//             case NIKE :
+//                 return new NikeShoes();
+//                 break;
+//             case LINING : 
+//                 return new LiNingShoes();
+//                 break;
+//             case ADIDAS :
+//                 return new AdidasShoes();
+//                 break;
+//             default :
+//                 return NULL;
+//                 break;
+//         }
+//     }
+// };
+
+// 工厂模式
+class ShoesFactory {
 public:
-    // 根据鞋子类型创建对应的鞋子对象
-    Shoes *CreateShoes(SHOES_TYPE type) {
-        switch (type) {
-            case NIKE :
-                return new NikeShoes();
-                break;
-            case LINING : 
-                return new LiNingShoes();
-                break;
-            case ADIDAS :
-                return new AdidasShoes();
-                break;
-            default :
-                return NULL;
-                break;
-        }
+    virtual Shoes* CreateShoes() = 0;
+    ~ShoesFactory();
+};
+
+class NikeProducer : public ShoesFactory {
+public: 
+    Shoes* CreateShoes() {
+        return new NikeShoes();
     }
 };
 
+class AdidasProducer : public ShoesFactory {
+public:
+    Shoes* CreateShoes() {
+        return new AdidasShoes();
+    }
+};
+
+class LiNingProducer : public ShoesFactory {
+public:
+    Shoes* CreateShoes() {
+        return new LiNingShoes();
+    }
+};
+
+// int main()
+// {
+//     // 构造工厂对象
+//     ShoesFactory shoesFactory;
+
+//     // 从鞋厂对象创建耐克鞋对象
+//     Shoes *pNikeShoes = shoesFactory.CreateShoes(NIKE);
+//     if (pNikeShoes != NULL) {
+//         pNikeShoes->show();
+//         // 释放资源
+//         delete pNikeShoes;
+//         pNikeShoes = NULL;
+//     }
+
+//     // 同理对于阿迪达斯和耐克
+//     Shoes *pAdidasShoes = shoesFactory.CreateShoes(ADIDAS);
+//     if (pAdidasShoes != NULL) {
+//         pAdidasShoes->show();
+//         // 释放资源
+//         delete pAdidasShoes;
+//         pAdidasShoes = NULL;
+//     }
+
+//     Shoes *pLingNingShoes = shoesFactory.CreateShoes(LINING);
+//     if (pLingNingShoes != NULL) {
+//         pLingNingShoes->show();
+//         //释放资源
+//         delete pLingNingShoes;
+//         pLingNingShoes = NULL;
+//     }
+
+//     return 0;
+// }
+
 int main()
 {
-    // 构造工厂对象
-    ShoesFactory shoesFactory;
+    // Nike 流水线
+    ShoesFactory *pnikeProducer = new NikeProducer();
+    // 生产线产出球鞋
+    Shoes *pnikeShoes = pnikeProducer->CreateShoes();
+    // show()
+    pnikeShoes->show();
+    // 释放资源
+    delete pnikeShoes;
+    delete pnikeProducer;
 
-    // 从鞋厂对象创建耐克鞋对象
-    Shoes *pNikeShoes = shoesFactory.CreateShoes(NIKE);
-    if (pNikeShoes != NULL) {
-        pNikeShoes->show();
-        // 释放资源
-        delete pNikeShoes;
-        pNikeShoes = NULL;
-    }
-
-    // 同理对于阿迪达斯和耐克
-    Shoes *pAdidasShoes = shoesFactory.CreateShoes(ADIDAS);
-    if (pAdidasShoes != NULL) {
-        pAdidasShoes->show();
-        // 释放资源
-        delete pAdidasShoes;
-        pAdidasShoes = NULL;
-    }
-
-    Shoes *pLingNingShoes = shoesFactory.CreateShoes(LINING);
-    if (pLingNingShoes != NULL) {
-        pLingNingShoes->show();
-        //释放资源
-        delete pLingNingShoes;
-        pLingNingShoes = NULL;
-    }
-
-    return 0;
 }
